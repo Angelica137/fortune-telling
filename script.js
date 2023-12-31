d38442e12e7aa9844fd1a96daf5271551de2b0cb
Angelica <Angelica137@users.noreply.github.com>
2023-12-31 18:57:23 +0000

    fix image tag and comment properly

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fortuneButton = document.getElementById("fortuneButton");
+
+  fortuneButton.addEventListener("click", () => {
+    alert("Button clicked!");
+  });
+});
