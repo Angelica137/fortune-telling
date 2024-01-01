d7af1b5c73ff9bfeaccaeef810baf8fb00409dbb
Angelica <Angelica137@users.noreply.github.com>
2024-01-01 11:55:54 +0000

    retrieve the card img names

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ async function loadCardData() {
     const data = await response.json();
     return data.cards;
   } catch (error) {
-    console.error("Error leading card data:", error);
+    console.error("Error loading card data:", error);
   }
+}
+
+async function getCardImg() {
+  const cards = await loadCardData();
+  const cardImgs = cards.map((card) => card.img);
+  console.log("Card images array", cardImgs);
+  return cardImgs;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,6 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Failed to load the cards");
     }
   });
+
+  getCardImg()
+    .then((cardImgs) => {
+      console.log("returned card images", cardImgs);
+    })
+    .catch((error) => {
+      console.error("Error in getCardImg:", error);
+    });
 
   const fortuneButton = document.getElementById("fortuneButton");
 
