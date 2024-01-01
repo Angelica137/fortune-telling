24a5fbeb10e9a8c25f507a8654608fed2f583b52
Angelica <Angelica137@users.noreply.github.com>
2024-01-01 11:35:21 +0000

    make sure I can load data from the json file

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+// fetching data from the JSON file to load it into JS file
+async function loadCardData() {
+  try {
+    const response = await fetch("tarot-images.json");
+    const data = await response.json();
+    return data.cards;
+  } catch (error) {
+    console.error("Error leading card data:", error);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  let cardData = [];
+
+  loadCardData().then((cards) => {
+    if (cards) {
+      cardData = cards;
+      console.log("Loaded cards:", cardData);
+    } else {
+      console.log("Failed to load the cards");
+    }
+  });
+
   const fortuneButton = document.getElementById("fortuneButton");
 
   fortuneButton.addEventListener("click", () => {
